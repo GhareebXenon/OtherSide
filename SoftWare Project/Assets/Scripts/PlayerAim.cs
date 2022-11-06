22754b33b77bf2973d1e2fa3fdfd25139002282d
GhareebXenon <strangemido11@yahoo.com>
2022-11-06 23:12:57 +0200

    Aim Semi-Done

--- SoftWare Project/Assets/Scripts/PlayerAim.cs
+++ SoftWare Project/Assets/Scripts/PlayerAim.cs
@@ -11,6 +11,7 @@ public class PlayerAim : MonoBehaviour
     public class OnShootEventArgs : EventArgs {
         public Vector3 gunEndPointPosition;
         public Vector3 ShotPosition;
+        public Vector3 shellPosition;
 
 
 
@@ -18,6 +19,7 @@ public class PlayerAim : MonoBehaviour
     // Start is called before the first frame update
     private Transform aimTransform;
     [SerializeField] private Transform GunEndPointTransform;
+    [SerializeField] private Transform shellPositionTransform;
     private Animator aimAnim;
 
 
@@ -39,9 +41,22 @@ public class PlayerAim : MonoBehaviour
     {
         Vector3 mousePosition = UtilsClass.GetMouseWorldPosition();
         //Follow the mouse in the z axis only
-        Vector3 aimDirection = (mousePosition - transform.position).normalized;
+        Vector3 aimDirection = (mousePosition - aimTransform.position).normalized;
         float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
         aimTransform.eulerAngles = new Vector3(0, 0, angle);
+
+        Vector3 aimlocalScale = Vector3.one;
+            if(angle > 90 || angle < -90)
+        {
+            aimlocalScale.y = -1f;
+
+        }else
+        {
+            aimlocalScale.y = +1f;
+
+        }
+        aimTransform.localScale = aimlocalScale; 
+            
         
 
     }
@@ -51,16 +66,19 @@ public class PlayerAim : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 mousePosition = UtilsClass.GetMouseWorldPosition();
-            aimAnim.SetTrigger("Shoot");
+            
+
             OnShoot?.Invoke(this, new OnShootEventArgs
             {
                 gunEndPointPosition = GunEndPointTransform.position,
-                ShotPosition = mousePosition
+                ShotPosition = mousePosition,
+                shellPosition = shellPositionTransform.position,
             }) ;
 
-
-
-
         }
+        if (Input.GetMouseButtonDown(0))
+            aimAnim.SetBool("Shoot", true);
+        if (Input.GetMouseButtonUp(0))
+            aimAnim.SetBool("Shoot", false);
     }
 }
