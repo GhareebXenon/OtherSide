f00cd09eb74cdbc34456e3e5e4866e8fef8bd6d8
reiyuchan <therealsyedofficial@gmail.com>
2022-11-08 01:18:51 +0200

    add new features

---
+++ SoftWare Project/Assets/Scripts/TerrainTile.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainTile : MonoBehaviour
+{
+    [SerializeField] Vector2Int tilePosition;
+    void Start()
+    {
+        GetComponentInParent<InfiniteWorld>().Add(gameObject, tilePosition);
+    }
+ 
+}
