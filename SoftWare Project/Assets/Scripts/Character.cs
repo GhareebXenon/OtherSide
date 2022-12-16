52b3b99d199b6ebbfadc3dc5105c64800b266c75
GhareebXenon <strangemido11@yahoo.com>
2022-12-16 16:39:20 +0200

    prisoncell

--- SoftWare Project/Assets/Scripts/Character.cs
+++ SoftWare Project/Assets/Scripts/Character.cs
@@ -1,9 +1,11 @@
 using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+    public static event Action OnPlayerDeath;
     public int maxHealth = 100;
     public int currentHealth;
     public HealthBar healthBar;
