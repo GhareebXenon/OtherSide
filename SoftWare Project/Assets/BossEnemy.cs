66e834a0997f1bdd44fe69f3352e385e8a2d7a46
JakeKroeger <JakyJake_Kroeger@hotmail.com>
2022-12-20 15:23:56 +0200

    Boss update V2

---
+++ SoftWare Project/Assets/BossEnemy.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossEnemy : MonoBehaviour
+{
+    public int damage = 5;
+    public Character healthBar;
+    Transform targetDestination;
+    GameObject targetGameobject;
+    [SerializeField] float speed;
+
+    Rigidbody2D rgdbd2d;
+
+    private void Awake()
+    {
+        rgdbd2d = GetComponent<Rigidbody2D>();
+    }
+
+    public void SetTarget(GameObject target)
+    {
+        targetGameobject = target;
+        targetDestination = target.transform;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 direction = (targetDestination.position - transform.position).normalized;
+        rgdbd2d.velocity = direction * speed;
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            healthBar.TakeDamge(damage);
+        }
+    }
+
+}
---
+++ SoftWare Project/Assets/BossManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossManager : MonoBehaviour
+{
+    [SerializeField] GameObject enemy;
+    [SerializeField] Vector2 spawnArea;
+    [SerializeField] GameObject player;
+    public float SpawnRate = 2f;
+    float nextSpawn = 0.0f;
+
+    public void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + SpawnRate;
+            SpawnEnemy();
+        }
+    }
+
+
+    public void SpawnEnemy()
+    {
+        Vector3 position = GenerateRandomPosition();
+
+        position += player.transform.position;
+
+        GameObject newEnemy = Instantiate(enemy);
+        newEnemy.transform.position = position;
+        newEnemy.GetComponent<BossEnemy>().SetTarget(player);
+        newEnemy.transform.parent = transform;
+    }
+
+    private Vector3 GenerateRandomPosition()
+    {
+        Vector3 position = new Vector3();
+
+        float f = UnityEngine.Random.value > 0.5f ? -1f : 1f;
+        if (UnityEngine.Random.value > 0.5f)
+        {
+            position.x = UnityEngine.Random.Range(-spawnArea.x, spawnArea.x);
+            position.y = spawnArea.y * f;
+        }
+        else
+        {
+            position.y = UnityEngine.Random.Range(-spawnArea.y, spawnArea.y);
+            position.x = spawnArea.x * f;
+
+        }
+
+        position.z = 0;
+
+        return position;
+    }
+}
